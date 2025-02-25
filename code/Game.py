250a12ac79cf64d4368dc96a4d536d9d5d69ddc5
ranieriiuri <iuriranierioliveira@gmail.com>
2025-02-25 11:18:14 -0300

    Implementing Menu option moves

--- code/Game.py
+++ code/Game.py
@@ -1,6 +1,7 @@
 import pygame
 
-from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from code.Level import Level
 from code.Menu import Menu
 
 
@@ -12,10 +13,13 @@ class Game:
     def run(self):
         while True:
             menu = Menu(self.window)
-            menu.run()
-            pass
-            #Check for all events
-            #for event in pygame.event.get():
-            #    if event.type == pygame.QUIT:
-            #        pygame.quit() #Close window
-            #        quit()
+            menu_return = menu.run()
+
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window, 'Level1', menu_return)
+                level_return = level.run()
+            elif menu_return == MENU_OPTION[4]:
+                pygame.quit()
+                quit()
+            else:
+                pass
